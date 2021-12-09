55505ec2b3c6ff4a736d35b61680d4b3a6f243bf
OMGitzFROST <deltoro.steve98@gmail.com>
2021-12-09 12:23:45 -0800

    Discord link now copies to clipboard

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/*
-	Solid State by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function ($) {
 
     var $window = $(window),
@@ -151,9 +145,3 @@
         });
 
 })(jQuery);
-
-window.onbeforeunload = () => {
-    for (const form of document.getElementsByTagName('form')) {
-        form.reset();
-    }
-}
